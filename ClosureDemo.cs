32767a2adf8d1e573a34b373270b56e8c99945b0
Danikopaniko <c.tar.daniiel@student,uzhnu.edu.ua>
2026-06-09 11:22:48 +0300

    добавлено ще 2 класси

---
+++ ClosureDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab7
+{
+    public class ClosureDemo
+    {
+        public static void Run()
+        {
+            Console.WriteLine("\n=== ЗАВДАННЯ 1: ЗАХОПЛЕННЯ КОНТЕКСТУ ===");
+
+            List<Action> actions = new List<Action>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int currentNumber = i;
+                actions.Add(() => Console.WriteLine($"Значення з замикання: {currentNumber}"));
+            }
+
+            foreach (var action in actions)
+            {
+                action();
+            }
+        }
+    }
+}
---
+++ DiscountManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab7
+{
+    public class DiscountManager
+    {
+        public static void Run()
+        {
+            Console.WriteLine("\n=== ЗАВДАННЯ 3: ЛАНЦЮЖОК ЗНИЖОК ===");
+
+            Func<double, double> discountChain = (price) => price * 0.95;
+            discountChain += (price) => price * 0.90;
+            discountChain += (price) => price - 100;
+
+            double startPrice = 1000;
+            double finalPrice = startPrice;
+
+            Delegate[] discounts = discountChain.GetInvocationList();
+
+            foreach (Func<double, double> discount in discounts)
+            {
+                finalPrice = discount(finalPrice);
+            }
+
+            Console.WriteLine($"Початкова ціна товару: {startPrice} грн.");
+            Console.WriteLine($"Ціна після всіх знижок (ланцюжком): {finalPrice} грн.");
+        }
+    }
+}
