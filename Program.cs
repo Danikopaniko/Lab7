a5453f00712e32b18ffc80b39bb0b5ba83f91447
Danikopaniko <c.tar.daniiel@student,uzhnu.edu.ua>
2026-06-09 11:30:28 +0300

    готова робота, при запуску показує що все працює

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab7
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            ClosureDemo.Run();
+
+            Console.WriteLine("\n=== ЗАВДАННЯ 2: ДЕЛЕГАТ ПРОТИ ПОДІЇ ===");
+
+            BankTerminal terminal = new BankTerminal();
+
+            terminal.OnMoneyWithdraw += (sum) => Console.WriteLine($"[SMS Notification] Знято: {sum} грн.");
+
+            terminal.OnMoneyWithdraw = null;
+            terminal.OnMoneyWithdraw?.Invoke(999999);
+
+            terminal.OnMoneyWithdrawSecure += (sum) => Console.WriteLine($"[Secure Notification] Знято: {sum} грн.");
+
+            // terminal.OnMoneyWithdrawSecure = null; 
+            // terminal.OnMoneyWithdrawSecure.Invoke(500);
+
+            terminal.Withdraw(500);
+
+            DiscountManager.Run();
+
+            Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
+            Console.ReadKey();
         }
     }
 }
