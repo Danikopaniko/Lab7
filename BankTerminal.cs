05f77b40edd8591dbf189093b69ab94e36e1cddb
Danikopaniko <c.tar.daniiel@student,uzhnu.edu.ua>
2026-06-09 11:21:08 +0300

    Добавьте файлы проекта.

---
+++ BankTerminal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab7
+{
+    public class BankTerminal
+    {
+        public Action<int> OnMoneyWithdraw;
+
+        public event Action<int> OnMoneyWithdrawSecure;
+
+        public void Withdraw(int amount)
+        {
+            Console.WriteLine($"[Термінал 27] Ініційовано зняття коштів: {amount} грн.");
+            OnMoneyWithdraw?.Invoke(amount);
+            OnMoneyWithdrawSecure?.Invoke(amount);
+        }
+    }
+}
